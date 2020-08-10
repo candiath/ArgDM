d639d6e7831d5f4657622da21f2064fac46f5136
Natán Candia <candianatan@fceqyn.unam.edu.ar>
2020-08-10 11:26:34 -0300

    prueba 1 monitor
    monitor de usuarios dropbear

--- users_source.sh
+++ users_source.sh
@@ -9,17 +9,23 @@ function listarOnlines {
 data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 
 declare -A connections
-        
+
 echo -e "Calculando...\n"
 for PID in "${data[@]}"
 do
-		#username=$(cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}')
-		username=$(cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk -F "'" '{print $2}')
+		username=$(cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}')
+#		username=$(cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | awk -F "'" '{print $2}')
 		# [ "$aux" = "" ]
 		# -n significa que la longitud del string no es cero
 
 		# Sumar
-		if [[ -n "$username"  ]]; then [ -n "${connections[$username]}" ] && connections[$username]=$((${connections[$username]} + 1)) || connections[$username]=1; fi
+		for item in ${username}; do
+			if [[ -n "$item"  ]]; then #Verificar si es necesario este paso
+				echo "ITEM ES $item"
+				[ -n "${connections[$item]}" ] && connections[$item]=$((${connections[$item]} + 1)) || connections[$item]=1
+			fi
+		done
+
 
 		#echo "check $PID";
         #NUM1=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
@@ -40,33 +46,33 @@ for index in ${!connections[*]}; do
 	#echo "$index - ${connections[$index]}\n"
 	printf "%-${espacio}s%s\n" $index ${connections[$index]}"/?"
 done
-sleep 2s
 }
 
+listarOnlines
 
-function createUser(){
+function createUser() {
 	#Validación pendiente del comando!!!
 	useradd -M -s /bin/false $1 ||
-	echo "Se ha producido un error al crear el usuario" &&
+	echo "Se ha producido un error al crear el usuario"
 	#$mainFuntion
 	# Tengo que buscar una mejor manera de volver al menú anterior!!!!!
 	# Acabo de notar que no es necesario volver al menú anterior (creo)
 	# Tendré que probar cómo funciona bash
 }
 
-function setPwd(){
+function setPwd() {
 	(echo $2; echo $2)|passwd $1
 	# Revisar qué errores pueden producirse acá
 }
 
-function setDays(){
+function setDays() {
 	# $name; $days
 	expDate=$(date '+%C%y-%m-%d' -d "+ $2 days")
 	usermod -e $expDate $1
 }
 
-function setLogins(){
-	# Active logins: 
+#function setLogins() {
+	# Active logins:
 	# ps -u $usur |grep sshd |wc -l
 	################################################
 	# Separador para formar columnas alineadas
@@ -74,6 +80,5 @@ function setLogins(){
 	# printf "%-${espacio}s%s" uno dos
 	# uno                           dosroot@VPS16:~#
 	################################################
-	
-}
 
+#}
